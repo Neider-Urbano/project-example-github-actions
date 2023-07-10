c950c6e7a1417b8b605b437ee148a0708c1df9a4
Neider Urbano <jjuly8julianur@gmail.com>
2023-07-10 11:15:10 -0500

    github actions aplicables a los pull request

--- src/node/server.js
+++ src/node/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const { version } = require("../../package.json");
 
 app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.send("Ok");
+});
+
+app.get("/version", (req, res) => {
+  res.send(version);
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome");
